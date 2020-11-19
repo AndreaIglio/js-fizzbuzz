934348bf8157ed1aff9cb9f0f297f0d4a61208f4
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-19 16:59:47 +0100

    Completed project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
-// Variabili con valore
+// FIZZBUZZ
+
+    // Stampa i numeri da 1 a 100
+
+for (var i = 1; i <= 100; i++) {
+
+    var number = i;
+
+    // se sono multipli di 3 e 5
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            console.log('FizzBuzz');
+        }
+        // se sono multipli di 5
+        else if (number % 5 == 0) {
+            console.log('Buzz');
+        }
+        // se sono multipli di 3
+        else if (number % 3 == 0) {
+            console.log('Fizz');
+        }
+        // se non sono multipli di 3 o 5 stampami il numero
+        else {
+            console.log(number);
+        }
 
 
-// Variabili senza valore
+       
+
+}
+
+
+
+// /FIZZBUZZ
+
+
+// var keep_going = true;
+
+// while (keep_going) {
+//     var keep_going = prompt (' inserisci il valore, se false smette ciclo');
+//     // APPARIRA' UN PROMPT, finche' noi non scriviamo 'false' il prompt continuera' ad apparire!!
+// }
+
+// do {
+//     var keep_going = prompt ('Inserisci valore, se false smeete ciclo');
+
+// } while (keep_going);
